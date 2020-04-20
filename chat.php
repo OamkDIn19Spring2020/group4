d685f43592bb1edfc5f6e177c96ba7b3f810d7dc
Dmitrii-Weaver <cordies1106@gmail.com>
2020-04-20 13:36:40 +0300

    minor changes and functions added

--- chat.php
+++ chat.php
@@ -15,37 +15,41 @@ include 'dbconnector.php';
         <h1> welcome <?php echo $_SESSION['name']  ?>!</h1>
         <div class="output">
 
-        <?php
-        $sql ="SELECT * FROM posts";
-        $result = $conn->query($sql);
+            <?php
+            $sql = "SELECT * FROM posts";
+            $result = $conn->query($sql);
 
-        if($result->num_rows > 0 ){
-            while ($row = $result->fetch_assoc()){
-                echo  "" . $row['name']. " " . "::" . $row['msg']. " -- " .$row['date']. "<br>" ;
-                echo "<br>";  
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo  "" . $row['name'] . " " . "::" . $row['msg'] . " -- " . $row['date'] . "<br>";
+                    echo "<br>";
+                }
+            } else {
+                echo "0 results";
             }
-        }
-        else {
-            echo "0 results";
-        }
 
-        $conn->close();
-        ?>
+            $conn->close();
+            ?>
 
 
 
 
 
         </div>
-
-        <form method="post"  action="send.php">
-        <textarea name='msg' placeholder='enter your message' class="form-control"></textarea> <br>
-        <input type="submit" value="send">
-        </form>
-        <br>
-        <form action="logout.php">
-
-            <input type="submit" value="logout" style="width: 100%">
-        </form>
+        <div id="sending">
+            <form method="post" action="send.php">
+                <input type="text" name='msg' id="message" placeholder='enter your message' class="form-control"> <br>
+                <script>
+                    var input = document.getElementById("message");
+                    input.addEventListener("keyup", function(event) {
+                        if (event.keyCode === 13) {
+                            $.get("send.php");
+                        }
+                    });
+                </script>
+                <input type="submit" value="send">
+            </form>
+            <br>
+        </div>
     </div>
 </body>
