ae073a5f24104bd9d40e5e8848519daddfc3b655
Dmitrii-Weaver <cordies1106@gmail.com>
2020-04-23 05:13:39 +0300

    deleted bad button

--- main.php
+++ main.php
@@ -12,7 +12,6 @@
 <body>
 
 <div class="contentbox">
-    <div class="login">Log In</div>
     <div class="maingrid">
         <div class="snakeframe">
             <iframe src="snake.php" name="snake" class ="snake"></iframe>
