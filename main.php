e584aefa2fb45660bca68599b6272ca18184f7e0
Dmitrii-Weaver <cordies1106@gmail.com>
2020-04-27 23:33:55 +0300

    added logout button

--- main.php
+++ main.php
@@ -1,7 +1,6 @@
-
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="styles.css">
@@ -11,19 +10,26 @@
 
 <body>
 
-<div class="contentbox">
-    <div class="maingrid">
-        <div class="snakeframe">
-            <iframe src="snake.php" name="snake" class ="snake"></iframe>
+
+    <div class="contentbox">
+        <div id="logoutsbutton">
+            <form action='logout.php' id="form">
+                <input type="submit" value="logout" id="logoutbutton">
+            </form>
         </div>
-        <div class="scoreframe">
-            <iframe src="topscores.php" name="topscores" class="topscores"></iframe>
-        </div>
-        <div class="chatframe">
-            <iframe src="chat.php" name="chat" class="chat"></iframe>
+        <div class="maingrid">
+            <div class="snakeframe">
+                <iframe src="snake.php" name="snake" class="snake"></iframe>
+            </div>
+            <div class="scoreframe">
+                <iframe src="topscores.php" name="topscores" class="topscores"></iframe>
+            </div>
+            <div class="chatframe">
+                <iframe src="chat.php" name="chat" class="chat"></iframe>
+            </div>
         </div>
     </div>
-</div>
 
 </body>
+
 </html>
---
+++ system/database/unholy.sql
@@ -0,0 +1,104 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: logins
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(200) NOT NULL,
+  `Password` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES (2,'user','passwd','name'),(3,'user1','user1','@'),(4,'sdupid','dumb','dumb@'),(5,'a','a','@');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `msg` varchar(200) NOT NULL,
+  `date` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'unreal','believe it',''),(2,'','ddd','2020-04-15 09:18:16'),(3,'','dd','2020-04-15 09:18:35'),(4,'a','','2020-04-19 18:20:50'),(5,'a','test','2020-04-19 18:24:19'),(6,'a','f','2020-04-19 18:43:55'),(7,'a','a','2020-04-19 18:44:49'),(8,'a','eew','2020-04-21 20:06:22'),(9,'a','awd','2020-04-21 20:09:43'),(10,'a','pawpawpaw','2020-04-21 20:24:27');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `scoreboard`
+--
+
+DROP TABLE IF EXISTS `scoreboard`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `scoreboard` (
+  `score_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(500) NOT NULL,
+  `score` varchar(500) NOT NULL,
+  PRIMARY KEY (`score_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `scoreboard`
+--
+
+LOCK TABLES `scoreboard` WRITE;
+/*!40000 ALTER TABLE `scoreboard` DISABLE KEYS */;
+INSERT INTO `scoreboard` VALUES (1,'TheMightyDmitrii','99999999'),(2,'Some dude','1');
+/*!40000 ALTER TABLE `scoreboard` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-23  4:02:40
